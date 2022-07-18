4a78288e6cc64a1b6de6e4aac63c63effbbd0c4c
Sorin C <sorin.cucereavu@gmail.com>
2022-07-18 12:28:49 +0300

    Interface name/surname repet

---
+++ src/test/java/repet/main.java
@@ -0,0 +1,31 @@
+package repet;
+interface nume
+         {
+      void nume ();
+         }
+         interface prenume
+         {
+      void prenume ();}
+             public class main {
+                 public static void main(String... ar) {
+                     id persoana1 = new id();
+                     persoana1.nume();
+                     persoana1.prenume();
+                 }
+
+             }
+             class id implements nume,prenume
+             {
+     @Override
+     public void prenume() {
+         System.out.println("Sorin");
+     }
+
+     @Override
+     public void nume() {
+         System.out.println("cucereavu");
+     }
+
+ }
+
+
